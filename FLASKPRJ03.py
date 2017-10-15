46fdaeb68c383af723305e8d9364146a808bee36
dev@silvatech.org <Password&2017>
2017-10-15 14:28:19 -0600

    Prelogin page setup seccessfully .

--- FLASKPRJ03.py
+++ FLASKPRJ03.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_login import LoginManager
+from flask_login import login_required
 
 app = Flask(__name__)
-
+login_manager = LoginManager(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/account')
+@login_required
+def account():
+    return "You are free"
 
 
 if __name__ == '__main__':
