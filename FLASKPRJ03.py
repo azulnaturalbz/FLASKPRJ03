3280c7ced232f4a347d158d6b3e3bd491fa0bc87
dev@silvatech.org <Password&2017>
2017-10-15 15:38:36 -0600

    Prelogin page setup seccessfully . MockDB and User Class Added and Imports to main page so far.

--- FLASKPRJ03.py
+++ FLASKPRJ03.py
@@ -1,8 +1,15 @@
 from flask import Flask,render_template
+from flask import redirect
+from flask import url_for
 from flask_login import LoginManager
 from flask_login import login_required
+from USER import User
+from MOCKDBHELPER import MockDBHelper as DBHelper
+
 
 app = Flask(__name__)
+app.secret_key = 'tPXJY3X37Qybz4QykV+hOyUxVQeEXf1Ao2C8upz+fGQXKsM'
+DB = DBHelper
 login_manager = LoginManager(app)
 
 @app.route('/')
