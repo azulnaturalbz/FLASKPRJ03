00d96c8bac91a6b22bdf9456911f425be58948fe
dev@silvatech.org <Password&2017>
2017-10-15 17:56:07 -0600

    Prelogin page setup seccessfully . MockDB and User Class Added and Imports to main page so far. Successfull login page completed so far . Makes great templating object for future uses. Added functionality log user out. Added password hashing and password. Adding users to mock users to mockdbhelper class . Verifying that password matches up to password in db. Added User registration from form corrected. Successful registration and login created for user.

--- PASSWORDHELPER.py
+++ PASSWORDHELPER.py
@@ -2,16 +2,14 @@ import hashlib
 import os
 import base64
 
-class PasswordHelper:
 
+class PasswordHelper:
 
     def get_hash(self,plain):
         return hashlib.sha512(plain).hexdigest()
 
-
     def get_salt(self):
-        return base64.b64decode(os.urandom(20))
-
+        return base64.b64encode(os.urandom(20))
 
     def validate_password(self, plain, salt, expected):
-        return self.get_hash(plain+salt) == expected
+        return self.get_hash(plain + salt) == expected
