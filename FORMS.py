aea5711b7b5a681ddbd427af5602e05406f0a60b
dev@silvatech.org <Password&2017>
2017-10-21 19:34:56 -0600

    Prelogin page setup seccessfully . MockDB and User Class Added and Imports to main page so far. Successfull login page completed so far . Makes great templating object for future uses. Added functionality log user out. Added password hashing and password. Adding users to mock users to mockdbhelper class . Verifying that password matches up to password in db. Added User registration from form corrected. Successful registration and login created for user. Html templating has taken placing using jinja and extention is occurring. Added routed pages html to the application. HTML layouts added and improved.
    Added Methods to the mockdbhelper class and more configuration was passed to the config file. A function was added to the main file as well. Function to remove table was added to page. Added Token to generate bitly link successfully.
    
    Successfully enable feature to remove pending request and added meta refresh. Added error message to registration fields and fixed all errors in the html and code.

--- FORMS.py
+++ FORMS.py
@@ -1,11 +1,11 @@
-from flask_wtf import form
+from flask_wtf import Form
 from wtforms import PasswordField
 from wtforms import SubmitField
 from wtforms.fields.html5 import EmailField
 from wtforms import validators
 
 
-class RegistrationForm(form):
+class RegistrationForm(Form):
     email = EmailField('email',validators=[validators.DataRequired(),
                                            validators.Email()])
     password = PasswordField('password',validators=[validators.DataRequired(),
